fd626181be0e0f9464bd0bc295b7ad6c332c3347
Max Magyar <magyarmr@gmatc.matc.edu>
2019-10-31 16:11:14 -0500

    App.tsx FInal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import './App.css';
-import { render } from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000')
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className ="App-header">
+        <header className="App-header">
           GoodThings
         </header>
+        {this.state.data}
       </div>
-    )
+    );
   }
 }
 
