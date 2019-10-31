f1d691babb5d59f09b23f7bbb7f0db4f913ed7c1
Max Magyar <magyarmr@gmatc.matc.edu>
2019-10-30 22:06:42 -0500

    Create a Registration API Endpoint

--- server.js
+++ server.js
@@ -7,10 +7,26 @@ const app = express();
 // Connect database
 connectDatabase();
 
-// API endpoints
+//Configure Middleware
+app.use(express.json({ extended: false}));
+
+//API Endpoints
+/**
+ * @route GET /
+ * @desc Test Endpoint
+ */
 app.get('/', (req, res) =>
 res.send('http get request sent to root api endpoint')
 );
 
-// Connection listener
+/**
+ * @route POST api/users
+ * @desc Register User
+ */
+app.post('api/users', (req, res) => {
+    console.log(req,body);
+    res.send(req, body);
+});
+
+//Connection Listener
 app.listen(5000, () => console.log('Express server running on port 5000'));
