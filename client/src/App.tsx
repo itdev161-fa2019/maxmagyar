89b25fdd86f298d1ddfc9cdd02780fe4fa13f5c8
Max Magyar <magyarmr@gmatc.matc.edu>
2019-11-13 10:43:26 -0600

    Completed Activity 8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,74 @@
-import React from 'react';
-import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
+import React from "react";
+import axios from "axios";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 
 class App extends React.Component {
   state = {
     data: null,
     token: null,
     user: null
-  }
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000')
-      .then((response) => {
+    axios
+      .get("http://localhost:5000")
+      .then(response => {
         this.setState({
           data: response.data
-        })
+        });
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(`Error fetching data: ${error}`);
-      })
+      });
+
+    this.authenticateUser();
   }
 
+  authenticateUser = () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      localStorage.removeItem("user");
+      this.setState({ user: null });
+    }
+
+    if (token) {
+      const config = {
+        headers: {
+          "x-auth-token": token
+        }
+      };
+      axios
+        .get("http://localhost:5000/api/auth", config)
+        .then(response => {
+          localStorage.setItem("user", response.data.name);
+          this.setState({ user: response.data.name });
+        })
+        .catch(error => {
+          localStorage.removeItem("user");
+          this.setState({ user: null });
+          console.error(`Error Logging in: ${error}`);
+        });
+    }
+  };
+
+  logOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({ user: null, token: null });
+  };
+
   render() {
+    let { user, data } = this.state;
+    const authProps = {
+      authenticateUser: this.authenticateUser
+    };
+
     return (
       <Router>
         <div className="App">
@@ -38,17 +82,38 @@ class App extends React.Component {
                 <Link to="/register">Register</Link>
               </li>
               <li>
-                <Link to="/login">Login</Link>
+                {user ? (
+                  <Link to="" onClick={this.logOut}>
+                    Log Out
+                  </Link>
+                ) : (
+                  <Link to="/login">Login</Link>
+                )}
               </li>
             </ul>
           </header>
           <main>
             <Route exact path="/">
-              {this.state.data}
+              {user ? (
+                <React.Fragment>
+                  <div>Hello {user}!</div>
+                  <div>{data}</div>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>Please Register or login</React.Fragment>
+              )}
             </Route>
             <Switch>
-              <Route path="/register" component={Register} />
-              <Route path="/Login" component={Login} />
+              <Route
+                exact
+                path="/register"
+                render={() => <Register {...authProps} />}
+              />
+              <Route
+                exact
+                path="/login"
+                render={() => <Login {...authProps} />}
+              />
             </Switch>
           </main>
         </div>
