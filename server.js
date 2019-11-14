f5c36611366d2fcb1c7e31fbb2848b26c66959ae
Max Magyar <magyarmr@gmatc.matc.edu>
2019-11-13 19:54:22 -0600

    Delete post endpoint

--- server.js
+++ server.js
@@ -239,7 +239,35 @@ app.get('/api/posts/:id', auth, async (req, res) => {
     res.json(post);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Server errror');
+    res.status(500).send('Server error');
+  }
+});
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ * 
+ */
+app.delete('/api/posts/:id' , auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if (!post) {
+      return res.status(400).json({ msg: 'Post not found' });
+    }
+
+    //Make sure the request user created the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized'});
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed'});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
   }
 });
 //connection listener
