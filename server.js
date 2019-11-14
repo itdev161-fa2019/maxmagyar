f8db485c97c5f53aa11c4ebdd6249de988d2f792
Max Magyar <magyarmr@gmatc.matc.edu>
2019-11-13 19:33:43 -0600

    Create an Endpoint to Get Posts

--- server.js
+++ server.js
@@ -208,6 +208,21 @@ app.post(
   }
 );
 
+/**
+ * @route GET api/posts
+ * @desc get posts
+ */
+app.get('/api/posts', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Serve error');
+  }
+});
+
 //connection listener
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
